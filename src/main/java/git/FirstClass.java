e3437295e383eb928787a475e705bfcde9f999b0
iosif ameziane <iosif.ameziane@gmail.com>
2020-11-19 22:16:27 +0200

    duplicate print in main method

--- src/main/java/git/FirstClass.java
+++ src/main/java/git/FirstClass.java
@@ -4,5 +4,8 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("We have first class");
+        System.out.println("Duplicate print method");
     }
+
+
 }
